c74aef378c8fdd9db0e930fdff6a50c6ff6ae9a3
MWats37 <130524759+MWats37@users.noreply.github.com>
2023-04-11 22:48:19 -0400

    Add files via upload

---
+++ Lab5/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Grading ID: K7989
+Lab 5
+Due 10/17/2021
+CIS 199 - 01
+This code asks for input from the user for temperatures. it will continue to ask for inputs until 999 is entered
+It will count the number of inputs and calculate the average at the end.*/
+using System;
+using static System.Console;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 0;
+            double avg;
+            int total = 0;
+            int count = 0;
+
+            do
+            {
+                WriteLine("Enter Temperature");
+                string input = ReadLine();
+                if (int.TryParse(input, out temp) && temp >= -20 && temp <= 130)
+                {
+                    total += temp;
+                    count++;
+                }
+                else if (temp != 999)
+                {
+                    WriteLine("Valid temperatures range from -20 to 130. Please reenter temperature");
+                }
+            } while (temp != 999);
+            WriteLine($"Number of Valid temperatures: {count}");
+            avg = total / count;
+            WriteLine($"The mean temperature is {avg}");
+          
+        }
+    }
+}
