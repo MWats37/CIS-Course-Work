a099dca462fa6d347d4088cd77bbe0419560e923
MWats37 <130524759+MWats37@users.noreply.github.com>
2023-04-11 22:42:27 -0400

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Grading ID:K7989, Lab 1, Due: 09/05/2021, CIS 199-01. This program will print out four
+ differnt lines containing my Grading ID, Hobbies, Favorite Book and Favorite Movie.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grading ID:     K7989");
+            Console.WriteLine("Hobbies:        Playing video games and exercising");
+            Console.WriteLine("Favorite Book:  The Way of Kings");
+            Console.WriteLine("Favorite Movie: Lord of the Rings");
+        }
+    }
+}
