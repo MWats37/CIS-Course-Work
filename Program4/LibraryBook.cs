0e8ddb7517ebeb06d285c2a949bca0e6b1c4e3f6
MWats37 <130524759+MWats37@users.noreply.github.com>
2023-04-11 22:52:15 -0400

    Add files via upload

---
+++ Program4/LibraryBook.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    class LibraryBook
+    {
+        private bool _isCheckedOut;
+        private string _bookTitle; //backing variables for the book title
+        private string _bookAuthor; //backing variables for the book author
+        private string _bookPublisher; //backing variables for the book publisher
+        private int _yearOfPublication; //backing variables for the year of publication
+        private string _iSBN; //backing variables for the ISBN
+
+        //Constructor for the Library book class
+        //precondition: all data entered are strings except for the year of publication
+        //postcondition: books with those properties created
+        public LibraryBook( string bookTitle, string bookAuthor, string bookPublisher, int yearOfPublication, string iSBN)
+        {
+            BookTitle = bookTitle;
+            BookAuthor = bookAuthor;
+            BookPublisher = bookPublisher;
+            YearOfPublication = yearOfPublication;
+            ISBN = iSBN;
+        }
+
+        //parameterless contructor
+        public LibraryBook()
+        {
+        }
+
+        public string BookTitle
+        {
+            //precondtion: None
+            //postcondition: book title returned
+            get
+            {
+                return _bookTitle;
+            }
+            //precondtion: value is entered
+            //postcondition: book title is set to entered value
+            set
+            {
+                _bookTitle = value;
+            }
+        }
+
+        
+        public string BookAuthor
+        {
+            //precondtion: None
+            //postcondition: book author returned
+            get
+            {
+                return _bookAuthor;
+            }
+            //precondtion: value is entered
+            //postcondition: book author is set to entered value
+            set
+            {
+                _bookAuthor = value;
+            }
+        }
+        
+        public string BookPublisher
+        {
+            //precondtion: None
+            //postcondition: book publisher returned
+            get
+            {
+                return _bookPublisher;
+            }
+            //precondtion: value is entered
+            //postcondition: book publisher is set to entered value
+            set
+            {
+                _bookPublisher = value;
+            }
+        }
+
+        
+        public int YearOfPublication
+        {
+            //precondtion: none
+            //postcondition: year of publication returned
+            get
+            {
+                return _yearOfPublication;
+            }
+            //precondtion: entered value is non negative
+            //postcondition: year of publication is set to specified value
+            set
+            {
+                if(value >= 0)
+                {
+                    _yearOfPublication = value;
+                }
+                else
+                {
+                    _yearOfPublication = 2019;
+                }
+            }
+        }
+
+        
+        public string ISBN
+        {
+            //precondtion: none
+            //postcondition: ISBN is returned
+            get
+            {
+                return _iSBN;
+            }
+            //precondtion: entered value
+            //postcondition: iSBN is set to entered vlaue
+            set
+            {
+                _iSBN = value;
+            }
+        }
+
+        //method to change the checkout status of the book to show that it has been checked out
+        public void CheckedOut()
+        {
+            _isCheckedOut = true;
+        }
+
+        //method to change the checkout status to reflect that the book has been returned
+        
+        public void ReturnToShelf()
+        {
+            _isCheckedOut = false;
+        }
+
+        //is checked out method to return the check out status of the book
+
+        public bool IsCheckedOut()
+        {
+            return _isCheckedOut;
+        }
+
+        //ToString method for book properties, overrides default tostring property
+        //Precondition: none
+        //Postcondition: a string for the book is returned
+        public override string ToString()
+        {
+            //book title, author, publisher, pubyear, isbn checkout
+            string nl = Environment.NewLine; // new line function
+            return $"Book - Title: {BookTitle}; Author: {BookAuthor}; Publisher: {BookPublisher}; Publication Year: {YearOfPublication}; ISBN: {ISBN}; Check Out Status: {IsCheckedOut()}; {nl}";
+        }
+    }
+}
---
+++ Program4/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Grading ID: K7987
+ * Program 4
+ * Due: 12/01/2021 (extension until 12/03/2021)
+ * CIS 199 - 01
+ * The purpose of this lab is to use classes and properties in order to change and display different data about library books, including title, author, publisher, publication year, ISBN, and check out status
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the 5 library book objects
+            LibraryBook book1 = new LibraryBook("To Kill a Mockingbird", "Harper Lee", "J. B. Lippincott & Co.", 1960, "9780060");
+            LibraryBook book2 = new LibraryBook("1984", "George Orwell", "Secker & Warburg", 1949, "9780451");
+            LibraryBook book3 = new LibraryBook("Fahrenheit 451", "Ray Bradbury", "Simon & Schuster", 1953, "9780564");
+            LibraryBook book4 = new LibraryBook("The Catcher in the Rye", "J. D. Salinger", "Little, Brown", 1951, "9790451");
+            LibraryBook book5 = new LibraryBook("The Way of Kings", "Brandon Sanderson", "Tor Books", 2010, "9740256");
+
+            //array for storying the objects
+            LibraryBook[] books = new LibraryBook[] { book1, book2, book3, book4, book5 };
+
+            //print out all books' original data
+            ListAllBooks(books);
+
+            //change the book information
+            book1.ISBN = "9845345"; // changing ISBN
+            book2.BookPublisher = "James Cameron"; //Changing the publisher
+            book3.CheckedOut(); //changing status to checked out
+            book4.ISBN = "9873214"; //changing the ISBN
+            book5.CheckedOut(); //changing status to checked out
+
+            //print out all new book data
+            ListAllBooks(books);
+
+            //calling the method ReturnToShelf to show that the books have been returned
+            book3.ReturnToShelf();
+            book5.ReturnToShelf();
+
+            //print out all the books data 
+            ListAllBooks(books);
+
+        }
+        //method to print out all books' data to console
+        private static void ListAllBooks(LibraryBook[] books)
+        {
+            foreach(LibraryBook book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
